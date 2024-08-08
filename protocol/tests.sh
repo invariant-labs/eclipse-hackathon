c19129606e6b5fc493feb378869fdc1623c1550d
Wojciech <cichockiwojciech97@gmail.com>
2024-08-08 19:54:55 +0200

    Add test.sh

---
+++ protocol/tests.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+e2e_tests=(
+    "init" 
+)
+
+# currenty, there is no unit tests 
+# cargo test
+
+# build protocol before run test
+./build.sh
+
+for test_cmd in "${e2e_tests[@]}"
+do
+    npm run "test:$test_cmd"
+done
