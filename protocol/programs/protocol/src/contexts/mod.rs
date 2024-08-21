819c53724678306c0f6d144c6aefbb978ffc9a79
none00y <mich_smyk@wp.pl>
2024-08-21 17:19:29 +0200

    remove unsused contexts

--- protocol/programs/protocol/src/contexts/mod.rs
+++ protocol/programs/protocol/src/contexts/mod.rs
@@ -1,17 +1,9 @@
 mod burn;
 mod init;
 mod init_pool;
-mod invoke_close_position;
-mod invoke_create_position;
-mod invoke_update_seconds_per_liquidity;
 mod mint;
-mod reopen_position;
 
 pub use burn::*;
 pub use init::*;
 pub use init_pool::*;
-pub use invoke_close_position::*;
-pub use invoke_create_position::*;
-pub use invoke_update_seconds_per_liquidity::*;
 pub use mint::*;
-pub use reopen_position::*;
