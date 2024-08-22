1de172225191b15208270ec9278d5fd70c656398
Piotr Matlak <piotr.matlak.contact@gmail.com>
2024-08-22 04:32:52 +0200

    unblock fee tiers

--- frontend/src/components/Liquidity/Liquidity.tsx
+++ frontend/src/components/Liquidity/Liquidity.tsx
@@ -93,7 +93,7 @@ export const Liquidity: React.FC<ILiquidity> = ({
   tickSpacing,
   // isWaitingForNewPool,
   poolIndex,
-  // bestTiers,
+  bestTiers,
   // canCreateNewPool,
   handleAddToken,
   commonTokens,
@@ -182,19 +182,16 @@ export const Liquidity: React.FC<ILiquidity> = ({
     return trimLeadingZeros(printBN(result, tokens[printIndex].decimals))
   }
 
-  // const bestTierIndex =
-  //   tokenAIndex === null || tokenBIndex === null
-  //     ? undefined
-  //     : (bestTiers.find(
-  //         tier =>
-  //           (tier.tokenX.equals(tokens[tokenAIndex].assetAddress) &&
-  //             tier.tokenY.equals(tokens[tokenBIndex].assetAddress)) ||
-  //           (tier.tokenX.equals(tokens[tokenBIndex].assetAddress) &&
-  //             tier.tokenY.equals(tokens[tokenAIndex].assetAddress))
-  //       )?.bestTierIndex ?? undefined)
-
-  // Temporary set best tier index as only available
-  const bestTierIndex = 2
+  const bestTierIndex =
+    tokenAIndex === null || tokenBIndex === null
+      ? undefined
+      : (bestTiers.find(
+          tier =>
+            (tier.tokenX.equals(tokens[tokenAIndex].assetAddress) &&
+              tier.tokenY.equals(tokens[tokenBIndex].assetAddress)) ||
+            (tier.tokenX.equals(tokens[tokenBIndex].assetAddress) &&
+              tier.tokenY.equals(tokens[tokenAIndex].assetAddress))
+        )?.bestTierIndex ?? undefined)
 
   const updatePath = (
     index1: number | null,
@@ -285,11 +282,6 @@ export const Liquidity: React.FC<ILiquidity> = ({
     return '0'
   }, [tokenADeposit, tokenBDeposit, poolIndex])
 
-  useEffect(() => {
-    if (bestTierIndex) {
-      setPositionTokens(tokenAIndex, tokenBIndex, bestTierIndex, true)
-    }
-  }, [bestTierIndex])
   return (
     <Grid container className={classes.wrapper} direction='column'>
       {showNoConnected && <NoConnected {...noConnectedBlockerProps} />}
